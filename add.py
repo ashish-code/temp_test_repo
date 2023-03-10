59c1ea29b60d0bc37df32c026e2b0b2fd48f5533
Ashish Gupta <ashishanilgupta@gmail.com>
2023-03-10 12:17:33 -0500

    Create add.py

---
+++ add.py
@@ -0,0 +1,9 @@
+def add(x,y):
+  return x+y
+
+if __name__ == '__main__':
+  print("Enter first number:")
+  x = input()
+  print("Enter second number:")
+  y = input()
+  print(f'Totoal is {add(x,y)}
